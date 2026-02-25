6d9cbddeae1a93670b9c96fea918d96b7fac68f7
Samy <gad.samy@outlook.com>
2026-02-26 00:12:26 +0100

    added tools.h for some more common helper functions

---
+++ tools.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+/// @brief function to find the minimal value in an array
+/// @param array 
+/// @param size size of array
+/// @return min value 
+int findMin(int* array, int size)
+{
+   
+    if(array == NULL)
+    {
+        fprintf(stderr,"Error is not initialized\n");
+        return -1;
+    }
+
+    int min = array[0];
+   
+
+    for (int i = 0; i < size; i++)  
+    {
+        if(*(array + i) < min)
+        {
+            min = *(array + i);
+        }
+    }
+    return min;
+}
+
+/// @brief function to find the minimal value in an array, ignoring -1
+/// @param array 
+/// @param size size of array
+/// @return min value 
+int findMinXn1(int* array, int size)
+{
+   
+    if(array == NULL)
+    {
+        fprintf(stderr,"Error is not initialized\n");
+        return -1;
+    }
+
+    int min = __INT_MAX__;
+   
+
+    for (int i = 0; i < size; i++)  
+    {
+        if(*(array + i) < min && *(array + i) != -1)
+        {
+            min = *(array + i);
+        }
+    }
+    return min;
+}
+
+/// @brief function to find the maximal value in an array
+/// @param array 
+/// @param size size of array
+/// @return max value 
+int findMax(int* array, int size)
+{
+   
+    if(array == NULL)
+    {
+        fprintf(stderr,"Error is not initialized\n");
+        return -1;
+    }
+
+    int max = array[0];
+   
+
+    for (int i = 0; i < size; i++)  
+    {
+        if(*(array + i) > max)
+        {
+            max = *(array + i);
+        }
+    }
+    return max;
+}
---
+++ tools.h
@@ -0,0 +1,6 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+int findMin(int*, int);
+int findMax(int*, int);
+int findMinXn1(int*, int);
+#endif 
