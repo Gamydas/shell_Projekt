ff9034e61772bbe02bcdf7e09720c07c5f9559a0
Samy <gad.samy@outlook.com>
2026-02-25 16:40:21 +0100

    implemented type(will be finished later) and (for now unused) echo

--- shellcmd.c
+++ shellcmd.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include "str.h"
 
 void cd(char* dir)
 {
     // if chdir failed i.e dir doesnt exist
     if(chdir(dir) == -1)
     {
-        printf("Directory does not exit.\n");
+        fprintf(stderr,"Directory does not exit.\n");
     }
 }
 
@@ -16,4 +17,20 @@ void pwd()
     char* temp = malloc(200);
     printf("%s\n", getcwd(temp,200));
     free(temp);
+}
+
+void echo(char* text)
+{
+    printf("%s\n", text);
+}
+
+void type(char* text)
+{
+    if(strcomp(text,"cd") == 0 || strcomp(text,"pwd") == 0 || strcomp(text,"echo") == 0 || strcomp(text,"type") == 0 )
+    {
+        printf("%s is a shell buildtin\n", text);
+    } else
+    {
+        fprintf(stderr,"%s: is not a buildtin\n", text); // a check for UNIX-Tools like ls & co. will be added later
+    }
 }
