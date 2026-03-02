4c2ac2aaf6f9b1b24724468bd982c0991a0d8e62
Samy <gad.samy@outlook.com>
2026-03-02 13:27:28 +0100

    added new parameters to shell struct

--- shell.h
+++ shell.h
@@ -1,6 +1,6 @@
 #include <termios.h>
 #include <unistd.h>
-#include "circBuff.h"
+#include "list.h"
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -10,10 +10,12 @@ typedef struct
     char cmd[200];                     // string for user command
     char* instruc[50];                 // array of strings for seperate instructions within the command
     char buffer[50][50];               // memory to assign to instruc, might be changed for dynamic memory in the future
-    charCircBuff history;
+
     struct termios canon;
     struct termios raw;
-
+    list history;
+    int latest;
+    int cursoridx;
 } shell;
 
 #endif
