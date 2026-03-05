1308d5ea3b58e94f8c96ebaa60029b20129cf6ab
Samy <gad.samy@outlook.com>
2026-03-05 20:20:54 +0100

    added a builtin array to shell struct to memorize all inplemented builtins upon initialization

--- shell.c
+++ shell.c
@@ -1,21 +1,30 @@
 #include "shell.h"
 #include "str.h"
+#include "tabComplete.h"
 
 /// @brief function initializes a shell struct
 /// @param sh 
 void initShell(shell* sh)
 {
-    sh->latest = 0;
     sh->cursoridx = 0;
-    sh->cmd[0] = '\0';
-    sh->in_history = 0;
     sh->doubletab = 0;
     sh->histpos = 0;
+    initStr(sh->cmd, 0, sizeof(sh->cmd));
+    initStr(sh->wdir, 0, sizeof(sh->wdir));
     for (int i = 0; i < 50; i++)
     {
+        initStr(sh->buffer[i], 0, sizeof(sh->buffer[i]));
         initStr(sh->hist[i], 0, sizeof(sh->hist[i]));
+        initStr(sh->builtins[i],0, sizeof(sh->builtins[0]));
     }
-    
+
+    // entering all currently existing builtins into the array
+    strcopy("cd", sh->builtins[0]);
+    strcopy("pwd", sh->builtins[1]);
+    strcopy("echo", sh->builtins[2]);
+    strcopy("type", sh->builtins[3]);
+    sh->amt = 4;
+
     tcgetattr(0, &sh->canon);
     tcgetattr(0, &sh->raw);
     sh->raw.c_iflag &= ~(IXON);
--- shell.h
+++ shell.h
@@ -1,6 +1,7 @@
 #include <termios.h>
 #include <unistd.h>
-//include "list.h"
+#include "tabComplete.h"
+
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -8,17 +9,20 @@ typedef struct
 {
     char wdir[2048];                   // string for working directory 
     char cmd[200];                     // string for user command
-    char* instruc[50];                 // array of strings for seperate instructions within the command
-    char buffer[50][50];               // memory to assign to instruc, might be changed for dynamic memory in the future
+    char* instruc[200];                 // array of strings for seperate instructions within the command
+    char buffer[50][200];               // memory to assign to instruc, might be changed for dynamic memory in the future
     char hist[50][200];                // array to store the command history in
+    char builtins[50][10];             // an array to store the name of all builtins
+    int amt;                           // indicates the amount of builtins
     int histpos;                       // index for hist array
     int cursoridx;                     // position index for the cursor on the screen
     int doubletab;                     // flag to check if there was a previous tab input
     struct termios canon;
     struct termios raw;
-    
+    tabComp compl;
     
 } shell;
+
 
 void initShell(shell* sh);
 #endif
