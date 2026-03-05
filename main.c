fbf1210ad8317de607d521058c3639b941f99869
Samy <gad.samy@outlook.com>
2026-03-05 16:53:41 +0100

    cleaned up main a bit

--- main.c
+++ main.c
@@ -9,31 +9,12 @@
 #include "str.h"
 #include "buildtins.h"
 #include "escapesequenzen.h"
-#include "list.h"
+//#include "list.h"
 
 int main(int argc, char *argv[])
 {
-    /*
-    char wdir[2048];                   // string for working directory
-    char cmd[200];                     // string for user command
-    char* instruc[50];                 // array of strings for seperate instructions within the command
-    char buffer[50][50];              // memory to assign to instruc, might be changed for dynamic memory in the future
-
-    struct termios canon;
-    struct termios raw;
-    tcgetattr(0, &canon);
-    tcgetattr(0, &raw);
-
-
-    */
     shell myShell;
-    myShell.latest = 0;
-    initList(&myShell.history);
-    tcgetattr(0, &myShell.canon);
-    tcgetattr(0, &myShell.raw);
-    myShell.raw.c_iflag &= ~(IXON);
-    myShell.raw.c_lflag &= ~(ECHO | ICANON | IEXTEN);
-
+    initShell(&myShell);
     CLEAR;
     HOME;
     while (1)
@@ -76,7 +57,6 @@ int main(int argc, char *argv[])
         // exits main function
         if (strcomp(myShell.cmd, "exit") == 0)
         {
-            freeList(&myShell.history);
             return 0;
         }
         if ((strcomp(myShell.instruc[0], "cd")) == 0)
@@ -104,7 +84,6 @@ int main(int argc, char *argv[])
             {
                 execvp(myShell.instruc[0], myShell.instruc);
                 fprintf(stderr, "%s: not a command\n", myShell.instruc[0]); // if execvp cannot find the given command
-                freeList(&myShell.history);
                 exit(1);                                                    // in case exec fails
             }
             else // parent
