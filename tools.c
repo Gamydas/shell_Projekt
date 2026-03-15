5bc0213d2eabdebcb8d69383b3e95b6802e5bb7a
Samy Gad <gad.samy@outlook.com>
2026-03-15 21:19:49 +0100

    new helper function to dynamically realloc char**

--- tools.c
+++ tools.c
@@ -1,4 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+/// @brief increases the capacity of char** i.e a sting array 
+/// @param array 
+/// @param capac 
+/// @return 0 if success, -1 if error
+int increaseCapacity(char** array, int* capac)
+{
+    (*capac) *= 2; // doubles given capacity
+    char** temp = realloc(array, *capac * sizeof(char*));
+    if (temp == NULL)
+    {
+        perror("realloc");
+        return -1;
+    }
+    array = temp;
+    return 0;
+
+}
+
 
 /// @brief function to find the minimal value in an array
 /// @param array 
