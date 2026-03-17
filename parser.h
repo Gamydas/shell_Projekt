b80bee3cae50ad5ced4d5c9e5861303c13efe4a8
Samy <gad.samy@outlook.com>
2026-03-17 13:52:12 +0100

    nicer formating

--- parser.h
+++ parser.h
@@ -10,31 +10,29 @@ typedef enum
 
 } MODUS;
 
-
 typedef enum
 {
-    NO_CONNECTOR, // value to use when initializing
-    PIPE,         // | pipe 
-    SEQ,          // ; sequential operator
-    END           // no connector found
+    NO_CONNECTOR,  // value to use when initializing
+    PIPE,          // | pipe
+    SEQ,           // ; sequential operator
+    END            // no connector found
 } CONNECTOR;
 
 typedef struct parsedInput
 {
-    char** args;         // array in which parsed tokens will be saved
-    int parseamt;       // current fill amount of parsed
-    int capac;          // capacity of parsed array, i.e amount of currently allocated slots
-    redirect redir[10]; // why would you ever need more than 10 in 1 cmd
-    int rdrctns;        // amount of redirectons
+    char **args;         // array in which parsed tokens will be saved
+    int parseamt;        // current fill amount of parsed
+    int capac;           // capacity of parsed array, i.e amount of currently allocated slots
+    redirect redir[10];  // why would you ever need more than 10 in 1 cmd
+    int rdrctns;         // amount of redirectons
 } Instructions;
 
-typedef struct 
+typedef struct
 {
-   Instructions* instructs;  // array of instructions, I.e every token until a connector is found
-   CONNECTOR* connects;       // array of connectors, connects[0] is the connector between instructs[0] and instructs[1]
-   int size;                 // size of given instruc list 
+    Instructions *instructs;  // array of instructions, I.e every token until a connector is found
+    CONNECTOR *connects;      // array of connectors, connects[0] is the connector between instructs[0] and instructs[1]
+    int size;                 // size of given instruc list
 } InstructList;
-
 
 int addSegment(InstructList *list, Instructions *instructs, CONNECTOR connect);
 int handleConnectors(Instructions *instructs, InstructList *list, CONNECTOR connect);
