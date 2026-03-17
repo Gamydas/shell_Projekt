720cab6a5a3228aaa373312e4f7ba184ad357216
Samy <gad.samy@outlook.com>
2026-03-17 12:08:18 +0100

    improved Redirection logic, now opens, dup2s and closes the fds for the caller (exec)

--- redirect.c
+++ redirect.c
@@ -1,24 +1,30 @@
+#include "redirect.h"
+
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <errno.h>
-#include "redirect.h"
+
 #include "err.h"
+#include "str.h"
 
 /// @brief           opens up filedesciptors for a given redirect object, closing needs
 ///                  to be handled by the calling function
 /// @param direction what type of redirect is called
 /// @param target    target of redirection
 /// @return          returns 0 if redirection was succesful, -1 otherwise
-int redirection(redirect *dir)
+int handleRedirections(redirect *dir)
 {
     ERR error = NO_ERROR;
     int *stream = &dir->stream;
-    char *target = &dir->target[0];
-    if (*target == NULL)
+    char *target = dir->target;
+    int cntrl = 0;
+    if (target == NULL)
     {
         error = INVALID_ARGUMENT;
         printError(error, "redirect");
+        return -1;
     }
     // checks which redirection was called and opens an approriate descriptor
     switch (dir->direction)
@@ -30,6 +36,15 @@ int redirection(redirect *dir)
             perror("open");
             return -1;
         }
+        // duplicates the new fd to designated channel and closes it after
+        cntrl = dup2(dir->stream, STDOUT_FILENO);
+        if (cntrl < 0)
+        {
+            perror("dup2");
+            return -1;
+        }
+        close(dir->stream);
+
         break;
     case REDIR_OUT_APPEND:
         *stream = open(target, O_WRONLY | O_CREAT | O_APPEND, 0644);
@@ -38,6 +53,15 @@ int redirection(redirect *dir)
             perror("open");
             return -1;
         }
+        // duplicates the new fd to designated channel and closes it after
+        cntrl = dup2(dir->stream, STDOUT_FILENO);
+        if (cntrl < 0)
+        {
+            perror("dup2");
+            return -1;
+        }
+        close(dir->stream);
+
         break;
     case REDIR_ERR_TRUNC:
         *stream = open(target, O_WRONLY | O_CREAT | O_TRUNC, 0644);
@@ -46,6 +70,15 @@ int redirection(redirect *dir)
             perror("open");
             return -1;
         }
+        // duplicates the new fd to designated channel and closes it after
+        cntrl = dup2(dir->stream, STDERR_FILENO);
+        if (cntrl < 0)
+        {
+            perror("dup2");
+            return -1;
+        }
+        close(dir->stream);
+
         break;
     case REDIR_ERR_APPEND:
         *stream = open(target, O_WRONLY | O_CREAT | O_APPEND, 0644);
@@ -54,6 +87,15 @@ int redirection(redirect *dir)
             perror("open");
             return -1;
         }
+        // duplicates the new fd to designated channel and closes it after
+        cntrl = dup2(dir->stream, STDERR_FILENO);
+        if (cntrl < 0)
+        {
+            perror("dup2");
+            return -1;
+        }
+        close(dir->stream);
+
         break;
 
     case REDIR_IN:
@@ -63,14 +105,22 @@ int redirection(redirect *dir)
             perror("open");
             return -1;
         }
+        // duplicates the new fd to designated channel and closes it after
+        cntrl = dup2(dir->stream, STDIN_FILENO);
+        if (cntrl < 0)
+        {
+            perror("dup2");
+            return -1;
+        }
+        close(dir->stream);
+
         break;
     default:
-        return -1; // should never be reached
+        return -1;  // should never be reached
     }
 
     return 0;
 }
-
 
 /// @brief switches the redirection mode and checks for syntax or logic errors
 /// @param direction redirection mode
@@ -79,14 +129,13 @@ int redirection(redirect *dir)
 /// @return 0 if succesful, -1 if an error occured
 int switchDirect(REDIR *direction, char *token, int size)
 {
-
     ERR error = NO_ERROR;
     if (size > 3)
     {
         error = SYNTAX_ERROR;
         // prints responsible char
         printError(error, &token[size - 1]);
-        return -1; // no redirection operator is greater than 3 characters
+        return -1;  // no redirection operator is greater than 3 characters
     }
 
     if (size == 0)
@@ -97,16 +146,15 @@ int switchDirect(REDIR *direction, char *token, int size)
 
     if (size > 0)
     {
-
-        switch (token[size - 1]) // always checking the prior operator
+        switch (token[size - 1])  // always checking the prior operator
         {
         case '>':
-            if (*direction == REDIR_OUT_TRUNC) // >>
+            if (*direction == REDIR_OUT_TRUNC)  // >>
             {
                 *direction = REDIR_OUT_APPEND;
                 return 0;
             }
-            else if (*direction == REDIR_ERR_TRUNC) // 2>>
+            else if (*direction == REDIR_ERR_TRUNC)  // 2>>
             {
                 *direction = REDIR_ERR_APPEND;
                 return 0;
@@ -120,12 +168,25 @@ int switchDirect(REDIR *direction, char *token, int size)
             }
             break;
         case '2':
-            *direction = REDIR_ERR_TRUNC; // 2>
+            *direction = REDIR_ERR_TRUNC;  // 2>
             return 0;
         default:
-            return -1; // this should never be reached
+            return -1;  // this should never be reached
             break;
         }
     }
-    return -1; // can never be reached, but makes the gcc happy :)
+    return -1;  // can never be reached, but makes the gcc happy :)
+}
+
+int setUpRedir(redirect *redir, char *target, REDIR *type)
+{
+    redir->direction = *type;
+    redir->target = calloc(1, strLen(target) + 1);  // +1 for \0
+    if (redir->target == NULL)
+    {
+        perror("malloc");
+        return -1;
+    }
+    strcopy(target, redir->target);
+    return 0;
 }
